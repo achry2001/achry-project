5f801157076d1ff5e8ad657e39e536463281065b
achry2001 <warwomen7@gmail.com>
2025-03-07 16:06:32 +0100

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 import { useAuth } from "@/hooks/useAuth";
-import { Circleinfo } from "lucide-react";
+import { CircleInfo } from "lucide-react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
